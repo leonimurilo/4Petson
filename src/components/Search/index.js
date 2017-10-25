b6117d58093b458f9471182cab642396dca0aad7
leoni <leonimega@hotmail.com>
2017-10-25 00:08:54 -0200

    add search component base structure

---
+++ src/components/Search/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import './styles.sass';
+
+class Search extends Component {
+  render() {
+    return null
+  }
+}
+
+export default Search;
