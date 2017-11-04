192d684f7a23af0001595f62c567cfca946f412d
leoni <leonimega@hotmail.com>
2017-11-04 17:05:49 -0200

    fix last commit

--- src/components/LocationPicker/index.js
+++ src/components/LocationPicker/index.js
@@ -94,7 +94,7 @@ const MapWithASearchBox = compose(
         style={{
           boxSizing: `border-box`,
           border: `1px solid transparent`,
-          width: `calc(50% - 200px)`,
+          width: `calc(100% - 200px)`,
           height: `32px`,
           marginTop: `10px`,
           padding: `0 12px`,
@@ -106,23 +106,6 @@ const MapWithASearchBox = compose(
         }}
       />
     </SearchBox>
-    <input
-      type="text"
-      placeholder="Select your location"
-      style={{
-        boxSizing: `border-box`,
-        border: `1px solid transparent`,
-        width: `calc(50% - 200px)`,
-        height: `32px`,
-        marginTop: `10px`,
-        padding: `0 12px`,
-        borderRadius: `3px`,
-        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-        fontSize: `14px`,
-        outline: `none`,
-        textOverflow: `ellipses`,
-      }}
-    />
     {props.markers.map((marker, index) =>
       <Marker key={index} position={marker.position} />
     )}
