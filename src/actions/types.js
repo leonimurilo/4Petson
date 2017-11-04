d3c5630ad4e4702657270bbcdb79876a17270f94
leoni <leonimega@hotmail.com>
2017-11-04 10:20:44 -0200

    add Auth reducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,5 +2,4 @@ export const USER_SIGNED_UP = "USER_SIGNED_UP";
 export const USER_LOGGED_IN = "USER_LOGGED_IN";
 export const USER_LOGGED_OUT = "USER_LOGGED_OUT";
 export const SET_AUTH_TOKEN = "SET_AUTH_TOKEN";
-export const CHECK_AUTH_TOKEN = "CHECK_AUTH_TOKEN";
 export const FETCH_SPECIES = "FETCH_SPECIES";
---
+++ src/reducers/Auth.js
@@ -0,0 +1,34 @@
+import {
+  SET_AUTH_TOKEN,
+  USER_LOGGED_IN,
+  USER_LOGGED_OUT,
+  USER_SIGNED_UP
+} from '../actions/types';
+
+const initialState = {
+  token: "",
+  user: {}
+}
+
+export default function (state = initialState, action) {
+  console.log("New action:", action.type);
+  switch (action.type){
+    case SET_AUTH_TOKEN: {
+      var newState = _.clone(state, true);
+      newState.token = action.payload;
+      return action.payload;
+    }
+    case USER_SIGNED_UP:
+    case USER_LOGGED_IN: {
+      return action.payload;
+    }
+    case USER_LOGGED_OUT: {
+      return {
+        token: "",
+        user: {}
+      };
+    }
+    default:
+      return state;
+  }
+}
