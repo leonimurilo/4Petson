25de8211b8f034b56ca0871bc14d411883b9c1c6
leoni <leonimega@hotmail.com>
2017-12-10 17:27:57 -0200

    delete announcement from home when deleting own announcement

--- src/reducers/Announcements.js
+++ src/reducers/Announcements.js
@@ -1,9 +1,14 @@
-import {FETCH_ANNOUNCEMENTS} from '../actions/types';
+import {FETCH_ANNOUNCEMENTS, DELETE_ANNOUNCEMENT} from '../actions/types';
 
 export default function (state = null, action) {
   switch (action.type){
     case FETCH_ANNOUNCEMENTS:
       return action.payload;
+    case DELETE_ANNOUNCEMENT:
+      let announcementId = action.payload
+      return state.filter(function(announcement){
+        return announcement.id != announcementId;
+      });
     default:
       return state;
   }
