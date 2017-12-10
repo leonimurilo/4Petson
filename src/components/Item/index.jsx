c1f9780d4462b909435d9e8955309199f88297e5
leoni <leonimega@hotmail.com>
2017-12-10 14:37:24 -0200

    finish home cards style

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -7,14 +7,24 @@ import './styles.sass';
 class Item extends Component {
   render() {
 
-    let url = require('../../assets/images/search.svg');
+    let url = require('../../assets/images/vectorpaint.svg');
+    try{
+      url = this.props.item.photos[0].url
+    }catch(e){
+      // console.log(e);
+    }
 
     return(
       <div className="item">
         <div className="content" onClick={()=>{
           browserHistory.push("/item/" + this.props.item.id);
         }}>
-          <img style={{width: "50px"}} src={url} />
+          <div>
+            <img src={url} />
+          </div>
+          <div className="resume">
+            <h4>{this.props.item.title}</h4>
+          </div>
         </div>
       </div>
     );
