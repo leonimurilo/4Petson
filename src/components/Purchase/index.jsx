163d704d137317b2058c32d55a1de801173aa7cb
leoni <leonimega@hotmail.com>
2017-12-10 16:54:41 -0200

    add validation to purchase component

--- src/components/Purchase/index.jsx
+++ src/components/Purchase/index.jsx
@@ -4,26 +4,43 @@ import './styles.sass';
 
 class Purchase extends Component {
   render() {
-    return (
-      <div className="trWrapper">
-        <div className="upper">
-          <div className="userImg" />
-          <div>
-            <h4>
-              <Link>{this.props.purchase.announcement.title}</Link>
-            </h4>
-            <div className="btnGroup">
-              <div className="tradeBtnWrapper lower">
-                <button className="normalBtn">Mais informações</button>
-              </div>
-              <div className="tradeBtnWrapper lower">
-                <button className="normalBtn">Ver anúncio</button>
+    console.log("purchase:", this.props.purchase);
+    if(this.props.purchase && this.props.purchase.announcement && this.props.purchase.announcement.title){
+      return (
+        <div className="trWrapper">
+          <div className="upper">
+            <div className="userImg" />
+            <div>
+              <h4>
+                <Link>{this.props.purchase.announcement.title}</Link>
+              </h4>
+              <div className="btnGroup">
+                <div className="tradeBtnWrapper lower">
+                  <button className="normalBtn">Mais informações</button>
+                </div>
+                <div className="tradeBtnWrapper lower">
+                  <button className="normalBtn">Ver anúncio</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }else{
+      return (
+        <div className="trWrapper">
+          <div className="upper">
+            <div className="userImg" />
+            <div>
+              <h4>
+                Anúncio indisponível
+              </h4>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
   }
 }
 
