341b735797b43b67931f52487eb877836bece5fd
leoni <leonimega@hotmail.com>
2017-12-10 18:47:47 -0200

    add announcements link in purchase component

--- src/components/Purchase/index.jsx
+++ src/components/Purchase/index.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 import './styles.sass';
 
 class Purchase extends Component {
+
+  onViewClick(event){
+    this.props.router.push("/item/"+this.props.purchase.announcement.id);
+  }
+
   render() {
     console.log("purchase:", this.props.purchase);
     if(this.props.purchase && this.props.purchase.announcement && this.props.purchase.announcement.title){
@@ -13,14 +18,16 @@ class Purchase extends Component {
             </div>
             <div>
               <h4>
-                <Link>{this.props.purchase.announcement.title}</Link>
+                <Link to={"item/"+this.props.purchase.announcement.id}>
+                  {this.props.purchase.announcement.title}
+                </Link>
               </h4>
               <div className="btnGroup">
                 <div className="tradeBtnWrapper lower">
                   <button className="normalBtn">Mais informações</button>
                 </div>
                 <div className="tradeBtnWrapper lower">
-                  <button className="normalBtn">Ver anúncio</button>
+                  <button className="normalBtn" onClick={this.onViewClick.bind(this)}>Ver anúncio</button>
                 </div>
               </div>
             </div>
@@ -45,4 +52,4 @@ class Purchase extends Component {
   }
 }
 
-export default Purchase;
+export default withRouter(Purchase);
